0738f703ea2f808950b0c5dc793019cf32886adc
pbk <impbk2002@naver.com>
2023-07-19 15:53:26 +0900

    사용하지 않는 코드 제거

--- src/main.py
+++ src/main.py
@@ -14,28 +14,19 @@ import threading
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-def initializer():
-    threading.current_thread().daemon = True
-
-def job():
-    while True:
-        time.sleep(1)
-        print('job')
 
 async def main():
     asyncio.get_running_loop().set_debug(True)
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-
-        while True:
-            print("enter command: ")
-            value = await aioconsole.ainput(loop=asyncio.get_running_loop())
-            if value == 'exit':
-                break
-            args = value.split(" ")
-            if args is None or len(args) < 1:
-                click.echo("wrong command input")
-                continue
-            await commands.run_command(args)
+    while True:
+        print("enter command: ")
+        value = await aioconsole.ainput(loop=asyncio.get_running_loop())
+        if value == 'exit':
+            break
+        args = value.split(" ")
+        if args is None or len(args) < 1:
+            click.echo("wrong command input")
+            continue
+        await commands.run_command(args)
     asyncio.get_running_loop().stop()
 
 
