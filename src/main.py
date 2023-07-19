fd60e794618b4d003e97c2d966f47ee11d871a01
pbk <impbk2002@naver.com>
2023-07-19 13:49:21 +0900

    commandline infinite application

--- src/main.py
+++ src/main.py
@@ -1,35 +1,62 @@
 # This is a sample Python script.
 import asyncio
-from typing import Final
-from Application import Application
-import aiorun
+import sys
+import threading
+import concurrent.futures.thread
 import uvloop
-import os, traceback, sys
+import aiorun
+import click
+import selectors
+import aioconsole
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-async def async_main() -> None:
-    print_hi('PyCharm')
+@click.group()
+def cli():
+    pass
 
 
-def print_hi(name) -> None:
+def run_command(args):
+    try:
+        cli.main(args=args, standalone_mode=False)
+    except click.exceptions.UsageError:
+        click.echo(sys.exception(), err=True)
+
+
+async def main():
+    asyncio.get_running_loop().set_debug(True)
+    while True:
+        print("enter command: ")
+        value = await aioconsole.ainput(loop=asyncio.get_running_loop())
+        if value == 'exit':
+            break
+        args = value.split(" ")
+        if args is None or len(args) < 1:
+            click.echo("wrong command input")
+            continue
+        await asyncio.to_thread(lambda: run_command(args))
+    asyncio.get_running_loop().stop()
+
+
+@cli.command()
+@click.argument("name")
+def print_hi(name="PyCharm") -> None:
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}', app.get_event_loop() is asyncio.get_running_loop())  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-app: Final[Application] = Application(debug=True)
+@cli.command()
+@click.option("--check", is_flag=True, default=False)
+def checkout(check="PyCharm") -> None:
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'check, {check}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    aiorun.run(
-        async_main(),
-        use_uvloop=True,
-        stop_on_unhandled_errors=True
-    )
-    # with app as park:
-    #     park(async_main())
-
+    aiorun.run(main(), use_uvloop=True, stop_on_unhandled_errors=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
