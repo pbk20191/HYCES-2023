f40f3160573628ad7089049f166b9e4cd96cd0b5
pbk <impbk2002@naver.com>
2023-07-20 11:27:59 +0900

    remove unnecessary venv path

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
         [
             "src/main.py",
             "-F",
-            "--path", ".venv/lib/python3.11/site-packages",
             "-n", "python_app",
         ]
     )
--- python_app.spec
+++ python_app.spec
@@ -6,7 +6,7 @@ block_cipher = None
 
 a = Analysis(
     ['src/main.py'],
-    pathex=['.venv/lib/python3.11/site-packages'],
+    pathex=[],
     binaries=[],
     datas=[],
     hiddenimports=[],
