7b7940a88be23a9dbc331e7043c175ddc6922d4b
park-byeong-gwan <impbk2002@gmail.com>
2023-08-08 18:44:47 +0900

    move pin factory context handling to application.py

--- src/Application.py
+++ src/Application.py
@@ -5,7 +5,8 @@ from typing import NoReturn
 import asyncio.exceptions
 import contextvars
 import inspect
-
+from gpiozero.devices import Device
+from contextlib import closing
 
 class ApplicationError(RuntimeError):
     pass
@@ -25,42 +26,44 @@ def ApplicationMain(entrypoint):
     def wrapper(*args, debug: bool | None = None, context: contextvars.Context | None = None, **kwargs) -> NoReturn:
         if threading.main_thread() is not threading.current_thread():
             raise ApplicationError("app.run() can only be called on main thread")
-        with asyncio.Runner(debug=debug) as runner:
-                queue = asyncio.Queue(1)
-                exception_handler = functools.partial(__handle_exception, queue)
-                runner.get_loop().set_exception_handler(exception_handler)
-                async def parking() -> None:
-                    while True:
-                        context = {}                        
-                        try:
-                            context: dict[str, any] = await queue.get()
-                        except asyncio.CancelledError:
-                            break
-                        try:
-                            exception: Exception | None = context.get("exception")
-                            if isinstance(exception, Exception):
-                                raise ApplicationError("uncaught exception occur on main event loop") from exception
-                            elif isinstance(exception, BaseException):
-                                raise exception
-                            else:
-                                asyncio.get_running_loop().default_exception_handler(context)
-                        finally:
-                            queue.task_done()
+        if Device.pin_factory is None:
+            Device.pin_factory = Device._default_pin_factory()
+        with asyncio.Runner(debug=debug) as runner, closing(Device.pin_factory):
+            queue = asyncio.Queue(1)
+            exception_handler = functools.partial(__handle_exception, queue)
+            runner.get_loop().set_exception_handler(exception_handler)
+            async def parking() -> None:
+                while True:
+                    context = {}                        
+                    try:
+                        context: dict[str, any] = await queue.get()
+                    except asyncio.CancelledError:
+                        break
+                    try:
+                        exception: Exception | None = context.get("exception")
+                        if isinstance(exception, Exception):
+                            raise ApplicationError("uncaught exception occur on main event loop") from exception
+                        elif isinstance(exception, BaseException):
+                            raise exception
+                        else:
+                            asyncio.get_running_loop().default_exception_handler(context)
+                    finally:
+                        queue.task_done()
 
-                async def __app__():
-                    if inspect.iscoroutinefunction(entrypoint):
-                        await asyncio.gather(entrypoint(*args, **kwargs), parking())
-                    else:
-                        await parking()
-                try:
-                    if not inspect.iscoroutinefunction(entrypoint):
-                        runner.get_loop().call_soon(functools.partial(entrypoint, *args, **kwargs), context=context)
-                    runner.run(coro=__app__(), context=context)
-                except RuntimeError as err:
-                    if err.args.count == 1 and err.args[0] == 'Event loop stopped before Future completed.':
-                        pass
-                    else:
-                        raise
+            async def __app__():
+                if inspect.iscoroutinefunction(entrypoint):
+                    await asyncio.gather(entrypoint(*args, **kwargs), parking())
+                else:
+                    await parking()
+            try:
+                if not inspect.iscoroutinefunction(entrypoint):
+                    runner.get_loop().call_soon(functools.partial(entrypoint, *args, **kwargs), context=context)
+                runner.run(coro=__app__(), context=context)
+            except RuntimeError as err:
+                if err.args.count == 1 and err.args[0] == 'Event loop stopped before Future completed.':
+                    pass
+                else:
+                    raise
         pass
     
     if inspect.iscoroutinefunction(entrypoint) or inspect.isfunction(entrypoint):
